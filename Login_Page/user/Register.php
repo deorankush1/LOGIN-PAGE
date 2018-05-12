0642a53b7e276083f697fc4f7b49a8784a88cf3f
Ankush Deora <ankushdeora11@gmail.com>
2018-05-12 22:37:36 +0530

    Update Register.php

--- Login_Page/user/Register.php
+++ Login_Page/user/Register.php
@@ -1,4 +1,6 @@
-<?php session_start();?>
+<?php session_start();
+//error_reporting(0); //disable all errors and notices
+?>
 
 <?php $error = $_SESSION['error'] ;?>
 
@@ -7,102 +9,186 @@
 
   <head>
     <style>
-    .error {color:brown;}
+    .error {color:red;}
     </style>
     <title> Signup </title>
 
     <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
   </head>  
-  
-
   <body>
-    	
-    <h4> <a href ='../admin/admin.html'>Login for Admin</a></h4> 
-    <pre style ="text-align: center;"> 
-      <h1>  Registration Page </h1>
-      <p><span class="error">*  Required field.</span></p>
-
-      
-      <form method="Post" action="../user/Registration.php" enctype="multipart/form-data" >  
-
-        firstName:   <input type="text" name ="firstname" placeholder= "ENTER FNAME" value = "<?php echo $error[0]['fname'] ? $error[0]['fname'] : ""; ?>"><span class="error">* </span><br>
+    <h4> <a href ='../admin/admin.html'>Login for Admin</a></h4><br><br>
+     
+    <h3 style ="text-align: center;">  Registration Page </h3><br>
+      <form style ="text-align: center;" method="Post" action="../user/Registration.php" enctype="multipart/form-data">      First Name: <input type="text" name ="firstname" placeholder= "Enter First Name" value = "<?php echo $error[0]['fname'] ? $error[0]['fname'] : ""; ?>"><span class="error">* </span>
         <?php 
          {
-         echo $error['firstname'];
-         echo '<br>';
-         
+          echo "<span class='error'>";
+         echo $error['firstname'];  
+         echo "</span>";
         }
-        ?>
-
-
-        lastName:     <input type="text" name="lastname"  placeholder= "ENTER LNAME" value = "<?php echo $error[1]['lname'] ? $error[1]['lname'] : ""; ?>" ><span class="error">* </span><br>
+        ?><br><br>
+        
+       Last Name: <input type="text" name="lastname"  placeholder= "Enter Last Name" value = "<?php echo $error[1]['lname'] ? $error[1]['lname'] : ""; ?>" ><span class="error">* </span>
         <?php
         { 
+        echo "<span class='error'>";
          echo $error['lastname'];
-         echo '<br>';
-         //session_destroy();
+         echo "</span>";
         }
-        ?>
-
-
-        email:       <input type="text" name="email"  placeholder= "ENTER E_ID " value = "<?php echo $error[2]['email1'] ? $error[2]['email1'] : ""; ?>" ><span class="error">* </span><br> 
+        ?><br><br>
+        
+        E-mail Id: <input type="text" name="email" id="email" onblur="checkMailStatus()" placeholder= "Enter Your Email_Id "  ><span class="error">* </span> <span id="emailstatus" name="estatus"></span>&nbsp;
+        <p id="demo"></p>
         <?php
         {
+        echo "<span class='error'>";
         echo $error['email'];
-        echo '<bprer>';
+        echo "</span>";
         //session_destroy();
-        }?>
-
-
-        password:    <input type="password" name="password"  placeholder= "ENTER PASSWORD" ><span class="error">* </span><br> 
+        }?><br><br>
+        
+        
+        Password: <input type="password" name="password"  placeholder= "Enter Password" value = "<?php echo $error[5]['PWD'] ? $error[5]['PWD'] : ""; ?>"><span class="error">* </span>
         <?php
         {
-        $error = $_SESSION['error'];
-         echo $error['password'];
-        echo '<br>';
+        //$error = $_SESSION['error'];
+        echo "<span class='error'>";
+        echo $error['password'];
+        echo "</span>";
+        //echo '<br><br>';
         //session_destroy();
-        }?>
+        }?><br><br>
 
-
-        re_password: <input type="password" name="re_password"  placeholder= "ENTER RE-PASSWORD" ><span class="error">* </span><br> 
-        age:         <input type = "varchar" name="age"  placeholder= "ENTER AGE" value = "<?php echo $error[3]['age1'] ? $error[3]['age1'] : ""; ?>" ><span class="error">* </span><br> 
+        Re-Enter-Password: <input type="password" name="re_password"  placeholder= "Enter Re-Enter-Password" ><span class="error">* </span><br><br>
+        
+        Age: <input type = "varchar" name="age"  placeholder= "Enter Age" value = "<?php echo $error[3]['age1'] ? $error[3]['age1'] : ""; ?>" ><span class="error">* </span> 
         <?php 
-        {
+        { 
+          echo "<span class='error'>";
           echo $error['age'];
-
-         echo '<br>';
+          echo "</span>";
          //session_destroy();
         }
-        ?>
+        ?><br><br>
 
-
-        Mobile:         <input type = "varchar" name="mobile"  placeholder= "ENTER MOBILE NUMBER" value = "<?php echo $error[4]['mbno'] ? $error[4]['mbno'] : ""; ?>"><span class="error">* </span><br>
+      MobileNumber: <input type = "varchar" name="mobile"  placeholder= "Enter Mobile Number" value = "<?php echo $error[4]['mbno'] ? $error[4]['mbno'] : ""; ?>"><span class="error">* </span>
         <?php 
          {
+         echo "<span class='error'>";
          echo $error['mobile'];
-         echo '<br>';
+         echo "</span>";
         $_SESSION['error'] = null;
         // session_destroy();
         }
-        ?>
+        ?><br><br>
 
+      Gender: <select name = gender>
+      <option value="select">Select</option>
+       <option value="Male">Male</option>
+       <option value="Female">Female</option>
+       <option value="0thers">others</option>
+      </select><span class="error">* </span>
+      <?php 
+        { 
+          echo "<span class='error'>";
+          echo $error['gender'];
+          echo "</span>";
+         //session_destroy();
+        }
+        ?><br><br>
+      
+      <input type="file" name="image">
+      <?php 
+        { 
+          echo "<span class='error'>";
+          echo $error['image'];
+          echo "</span>";
+         //session_destroy();
+        }
+        ?><br><br>
 
-        gender:  <select name = gender>
-           <option value="Male">Male</option>
-           <option value="Female">Female</option>
-           <option value="0thers">others</option>
-           </select>
-
-
-         Select image to upload: <input type="file" name="image"/>
-
-        <input type="Submit" value='submit' name='submit' class="btn btn-info" >
-
-        Already I have an account: <a href="../user/loginpage.html" class="btn btn-primary">Login</a>
+        <input type="Submit" value='submit' name='submit' class="btn btn-info" ><br><br>
+         <b>Already Registered</b>
+         <a href="../user/loginpage.html" class="btn btn-primary">Login</a>
       </form>
     </pre>
 
   </body>
 </html>
-<html oncontextmenu="return false">
-</html>
+<script type="text/javascript">
+  function checkMailStatus(){
+    var email=$("#email").val();// value in field email
+    var emaildata = localStorage.getItem(email);
+    console.log(email);
+    //console.log('emaildata');
+     if (emaildata == null)
+     {
+
+       $.ajax({
+        url:'../user/checkemail.php', // put your real file name 
+        type : 'POST',
+        dataType: 'json',
+        data:{user_email:email},
+        success:function(msg){         
+        //var obj = JSON.parse(msg);
+        //console.log(msg);
+      //   window.test=JSON.stringify(msg);
+         console.log(msg.email);
+         //alert(msg.msg);
+         $("#demo").html(msg.msg);
+        localStorage.setItem(email,(msg.msg));
+      //console.log(localStorage.getItem(email));
+      }
+    });
+    }else{
+     $("#demo").html(emaildata,'---');      
+     console.log(emaildata,'-----');
+  }
+  }
+</script>
+
+
+<!-- <script type="text/javascript">
+  var flag=1;
+function checkMailStatus(){
+//alert("came");
+var email=$("#email").val();// value in field email
+var emaildata = localStorage.getItem("emailcheck");
+var emailparsedata = JSON.parse(emaildata);
+console.log(emaildata.email);
+// json_decode(emaildata);
+console.log("here");
+ //console.log(e.email);
+ console.log("here");
+//  if (emaildata)
+//  {
+//    console.log(emaildata);
+//  }
+//  else{
+$.ajax({
+  url:'../user/checkmail.php', // put your real file name 
+    type : 'POST',
+    data:'user_email='+email,
+    success:function(msg){
+         
+         var obj = JSON.parse(msg);
+         console.log(obj.email);
+         window.test=JSON.stringify(msg);
+         
+          console.log(obj);
+        localStorage.setItem('emailcheck',JSON.stringify(obj));
+       
+        }
+
+ });
+ // console.log(localStorage.getItem("emailcheck"));
+ console.log( "emailcheck = " + localStorage.getItem("emailcheck"));
+ console.log("x");
+ 
+ console.log("x");
+//}
+}
+
+</script> -->
+
